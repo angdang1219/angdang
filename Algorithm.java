637d53d1ed7538b389f4741f67767c10026ec507
angdang1219 <54886853+angdang1219@users.noreply.github.com>
2019-09-22 00:49:13 +0800

    Update Algorithm.java

--- Algorithm.java
+++ Algorithm.java
@@ -1,7 +1,7 @@
 public class Algorithm{
 	public static void main(String[] args){
 		int count = 10;
-		count * = count + 2;
+		count = count + 2;
 		count/= 100;
 		count++;
 		System.out.println(count);
